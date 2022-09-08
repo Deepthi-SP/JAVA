9a8e14cc6d2905616ce2fb79d1a8361565c913a1
Deepthi-SP <100423337+Deepthi-SP@users.noreply.github.com>
2022-09-08 15:06:50 +0530

    Add files via upload

---
+++ prg6.java
@@ -0,0 +1,67 @@
+class Q{
+    int n;
+    boolean valueSet=false;
+    synchronized int get(){
+        while(!valueSet)
+        try{
+            wait();
+        }
+         catch (InterruptedException e) {
+          System.out.println("InterruptedException caught");
+        }
+        System.out.println("Got:"+n);
+        valueSet=false;
+        notify();;
+        return n;
+    }
+    synchronized void put(int n)
+    {
+        while(valueSet)
+        try {
+            wait();
+        } 
+        catch (InterruptedException e) {
+            System.out.println("InterruptedException caught");
+        }
+        this.n=n;
+        valueSet=true;
+        System.out.println("Put:"+n);
+        notify();
+    }
+}
+class Procedure implements Runnable{
+    Q q;
+    Procedure(Q q){
+        this.q=q;
+        new Thread(this,"procedure").start();
+    }
+    public void run(){
+        int i=0;
+        while(true){
+            q.put(i++);
+        }
+    }
+}
+class Consumer implements Runnable{
+    Q q;
+    Consumer(Q q){
+        this.q=q;
+        new Thread(this,"Consumaer").start();
+    }
+    public void run(){
+        int i=0;
+        while(true){
+            q.get();
+        }
+    }
+}
+public class prg6 {
+    public static void main(String[] args) {
+        Q q=new Q();
+        new Procedure(q);
+        new Consumer(q);
+        System.out.println("press Control-C to stop");
+            
+      }
+}
+
